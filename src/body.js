8934dc10fe4e2a29a0bb759e56164e2b9038a827
Chukwualuka Chiama <lukaschiama@gmail.com>
2018-10-29 21:04:28 +0100

    add images to src folder

--- src/body.js
+++ src/body.js
@@ -1,38 +1,46 @@
 import React from 'react';
 import { Carousel } from 'react-responsive-carousel';
+import wedding from './jeremy.jpg';
+import mountain_wedding from './jeremy.jpg';
+import drummer from './nathaniel.jpg';
+import biggy from './biggy.jpeg';
+import children from './martin.jpg';
+import food from './food1.jpeg';
+import dance from './dance1.jpg';
+import tractor from './tractor.jpg';
 
 export default () => (
   <Carousel autoPlay infiniteLoop width='800px' centerMode transitionTime={500}>
     <div>
-      <img src='/img/wedding.jpg' alt='wedding'/>
+      <img src={wedding} alt='wedding'/>
       <p className='legend'>A unique I do event?</p>
     </div>
     <div>
-      <img src='/img/jeremy.jpg' alt='mountain wedding'/>
+      <img src={mountain_wedding} alt='mountain wedding'/>
       <p className='legend'>Yeah, scream it from the rooftops?</p>
     </div>
     <div>
-      <img src='/img/nathaniel.jpg' alt='man drumming'/>
+      <img src={drummer} alt='man drumming'/>
       <p className='legend'>We agree the world should hear your voice!</p>
     </div>
     <div>
-      <img src='/img/biggy.jpeg' alt="tape around a man's stomach"/>
+      <img src={biggy} alt="tape around a man's stomach"/>
       <p className='legend'>Even if you'd rather go veggie</p>
     </div>
     <div>
-      <img src= '/img/martin.jpg' alt='children with a camera'/>
+      <img src={children} alt='children with a camera'/>
       <p className='legend'>We will always cover you!</p>
     </div>
     <div>
-      <img src='/img/food1.jpeg' alt='roasted plantains'/>
+      <img src={food} alt='roasted plantains'/>
       <p className='legend'>Whether you're at this</p>
     </div>
     <div>
-      <img src='/img/dance1.jpg' alt='dancers'/>
+      <img src={dance} alt='dancers'/>
       <p className='legend'>Or at this!</p>
     </div>
     <div>
-      <img src='/img/tractor.jpg' alt='drone watching tractor pulling people'/>
+      <img src={tractor} alt='drone watching tractor pulling people'/>
       <p className='legend'>We see you and we will make your day MEMORY-able!</p>
     </div>
   </Carousel>
